d9f270b8875e1eb147a8ba7dd376cfbe4be630e0
susan <bluenote@outlook.com>
2018-12-11 15:10:35 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  # Add your post route and action below
+ post '/team' do
+ 	#do instance variables here, then can show them in erb
+ 	 @name = params[:name]
+   @coach = params[:coach]
+   @point_guard = params[:pg]
+   @shooting_guard = params[:sg]
+   @small_forward = params[:sf]
+   @power_forward = params[:pf]
+   @center= params[:c]
+   erb :team
+ end
 
 end
