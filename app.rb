babf2aef29a04f313f768b71d7e461e0253522e7
jriv95 <joel.rivera7995@gmail.com>
2018-12-13 09:33:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  # so after creating the GET request from the newteam.erb file, we need a POST request to save the info and show it
+  post '/newteam' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    erb :team
+  end
+
 
 end
