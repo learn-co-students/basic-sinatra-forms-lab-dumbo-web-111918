f7ac835d1d2cdd81a3bb74b09e84916a2535de17
github username <github email address>
2018-12-14 09:44:58 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+get '/newteam' do
+  erb :newteam
 
+end
 
+ put "team" do
+   @team = Team.create(params[:team])
+    redirect to "/team/#{@team.id}"
+    erb :team
+
+ end
 end
